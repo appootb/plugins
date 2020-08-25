51274fd4ba722b17616c0c4c9b85baa80b8529d0
Yuriy Lu <youlu@outlook.com>
2020-08-25 20:30:19 +0800

    Add: account auth & task locker

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/appootb/plugins
 go 1.14
 
 require (
-	github.com/appootb/substratum v0.0.0-20200802131559-94e3fbcf5e05 // indirect
-	github.com/go-redis/redis v6.15.8+incompatible // indirect
+	github.com/appootb/protobuf/go v0.0.0-20200729151134-0f07fd3bead3
+	github.com/appootb/substratum v0.0.0-20200824132356-fcb9f81943ca
+	github.com/go-redis/redis v6.15.8+incompatible
 )
 
 replace google.golang.org/grpc => google.golang.org/grpc v1.26.0
---
+++ task/locker/redis/locker.go
@@ -0,0 +1,151 @@
+package redis
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/appootb/substratum/storage"
+	"github.com/appootb/substratum/task"
+	"github.com/appootb/substratum/util/hash"
+	"github.com/appootb/substratum/util/random"
+	"github.com/go-redis/redis"
+)
+
+const (
+	RandomValueLength  = 20
+	LockerTouchTimeout = time.Second * 3
+)
+
+const (
+	TaskLockerKey = "task:scheduler:locker:%s"
+)
+
+var (
+	touchScript = `
+	if redis.call("GET", KEYS[1]) == ARGV[1] then
+		return redis.call("expire", KEYS[1], ARGV[2])
+	else
+		return 0
+	end
+`
+
+	deleteScript = `
+	if redis.call("GET", KEYS[1]) == ARGV[1] then
+		return redis.call("DEL", KEYS[1])
+	else
+		return 0
+	end
+`
+)
+
+var (
+	Locker = &locker{}
+)
+
+func init() {
+	task.RegisterLockerImplementor(Locker)
+}
+
+func InitRedis(s storage.Storage) {
+	Locker.caches = s.GetRedisz()
+}
+
+type mutexData struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	key   string
+	value string
+}
+
+type locker struct {
+	mutex  sync.Map
+	caches []redis.Cmdable
+}
+
+// Get the locker of the scheduler,
+// should be blocked before acquired the locker.
+func (l *locker) Lock(ctx context.Context, scheduler string) context.Context {
+	mutex := &mutexData{
+		key:   fmt.Sprintf(TaskLockerKey, scheduler),
+		value: random.String(RandomValueLength),
+	}
+	mutex.ctx, mutex.cancel = context.WithCancel(ctx)
+	idx := hash.Sum(mutex.key) % int64(len(l.caches))
+
+	for {
+		reply, err := l.caches[idx].SetNX(mutex.key, mutex.value, LockerTouchTimeout*2).Result()
+		if err != nil || !reply {
+			time.Sleep(LockerTouchTimeout)
+		} else {
+			l.mutex.Store(scheduler, mutex)
+			go l.touch(mutex)
+			break
+		}
+	}
+
+	return mutex.ctx
+}
+
+// Give up the schedule locker.
+func (l *locker) Unlock(scheduler string) {
+	v, ok := l.mutex.Load(scheduler)
+	if !ok {
+		return
+	}
+	mutex := v.(*mutexData)
+	idx := hash.Sum(mutex.key) % int64(len(l.caches))
+	status, err := l.caches[idx].Eval(deleteScript, []string{mutex.key}, mutex.value).Bool()
+	if err != nil {
+		// TODO err
+	}
+	if !status {
+		// TODO failed
+	}
+	mutex.cancel()
+}
+
+func (l *locker) touch(mutex *mutexData) {
+	ticker := time.NewTicker(LockerTouchTimeout)
+
+	for {
+		select {
+		// Unlocked or parent context canceled.
+		case <-mutex.ctx.Done():
+			return
+
+		case <-ticker.C:
+			err := l.renew(mutex)
+			if err != nil {
+				mutex.cancel()
+				return
+			}
+		}
+	}
+}
+
+func (l *locker) renew(mutex *mutexData) error {
+	var (
+		err   error
+		reply bool
+	)
+	idx := hash.Sum(mutex.key) % int64(len(l.caches))
+
+	for i := 0; i < 3; i++ {
+		reply, err = l.caches[idx].Eval(touchScript, []string{mutex.key}, mutex.value, LockerTouchTimeout*2).Bool()
+		if err != nil {
+			time.Sleep(time.Second)
+			continue
+		}
+		if reply {
+			return nil
+		} else {
+			return errors.New("unlocked")
+		}
+	}
+
+	return err
+}
