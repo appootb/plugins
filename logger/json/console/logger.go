b5fd5407edb10a6d9775d2f0b9cdf480ed17afdd
Yuriy Lu <youlu@outlook.com>
2022-07-05 23:15:52 +0800

    Update std logger plugin

--- logger/json/console/logger.go
+++ logger/json/console/logger.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/appootb/substratum/v2/logger"
 	"github.com/appootb/substratum/v2/proto/go/common"
+	"github.com/appootb/substratum/v2/proto/go/secret"
 )
 
 func init() {
@@ -26,20 +27,45 @@ func (l *stdJSON) Log(level logger.Level, md *common.Metadata, msg string, c log
 	if int32(level) < atomic.LoadInt32(&l.level) {
 		return
 	}
-	//
 	writer := os.Stdout
 	if level >= logger.ErrorLevel {
 		writer = os.Stderr
 	}
 	//
-	if c == nil {
-		c = make(logger.Content, 3)
+	var outer logger.Content
+	if c != nil {
+		outer = make(logger.Content, len(c)+3)
+	} else {
+		outer = make(logger.Content, 3)
 	}
-	c["_MSG_.message"] = msg
-	c["_MSG_.metadata"] = md
-	c["_MSG_.level"] = level.String()
+	outer["LEVEL"] = level.String()
+	outer["MESSAGE"] = msg
+	outer["METADATA"] = md
+	if c == nil {
+		goto Print
+	}
 	//
-	content, err := json.Marshal(c)
+	for k, v := range c {
+		switch k {
+		case logger.LogConsumed:
+			outer["CONSUMED"] = v
+		case logger.LogPath:
+			outer["PATH"] = v
+		case logger.LogRequest:
+			outer["REQUEST"] = v
+		case logger.LogResponse:
+			outer["RESPONSE"] = v
+		case logger.LogSecret:
+			outer["UID"] = v.(*secret.Info).GetAccount()
+		case logger.LogError:
+			outer["ERROR"] = v
+		default:
+			outer[k] = v
+		}
+	}
+
+Print:
+	content, err := json.Marshal(outer)
 	if err != nil {
 		_, _ = fmt.Fprintln(writer, "std_json marshal failed:", err.Error())
 	} else {
